f111c9199ce71a8730aae9484680f4abdeb6963e
Josh Schwartzman <geeosh@gmail.com>
2012-06-12 09:31:58 -0700

    fix sess

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Nreduce::Application.config.session_store :cookie_store, key: '_nreduce_sess'
+Nreduce::Application.config.session_store :cookie_store, key: '_nreduce_sess', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
