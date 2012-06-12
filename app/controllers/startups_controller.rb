ec30da8a2de39130db22d713ba87bdd3cd3cd35e
Josh Schwartzman <geeosh@gmail.com>
2012-06-11 20:39:21 -0700

    Fix for search

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -25,7 +25,7 @@ class StartupsController < ApplicationController
     @search = session[:search] || {}
 
     # Establish basic query to find public startups
-    @startups = Startup.is_public.order(:name).includes(:meeting).paginate(:page => params[:page] || 1, :per_page => 10)
+    @startups = Startup.is_public.where(:onboarding_step => Startup.num_onboarding_steps).order(:name).includes(:meeting).paginate(:page => params[:page] || 1, :per_page => 10)
 
     # Add conditions
     # Ignore current user's startup
