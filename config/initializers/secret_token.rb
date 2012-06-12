eaf58be35657f728a83820ef2986bb61711427d2
Josh Schwartzman <geeosh@gmail.com>
2012-06-12 02:03:31 -0700

    Change secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Nreduce::Application.config.secret_token = '91dcc4114e1d33a87c69afc93ebc42a67f3c7f723234c88fafc197e3ec40741d6f66e674f5ad5f5511fd21913bf92052959e00582475974829cc0a78fe4a1369'
+Nreduce::Application.config.secret_token = '91dcc4114e1d33a87c69afc93ebjkf33ldsa023k22c88fafc197e3ec40741d6f66e674f5ad5f5511fd21913bf92052959e00582475974829cc0a78fe4a1369'
