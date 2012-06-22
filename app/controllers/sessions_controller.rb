5e6c965fe5ace8dc78c0900e82477e61a7f2e0d5
Josh Schwartzman <geeosh@gmail.com>
2012-06-22 15:22:51 -0700

    fix join email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,13 @@
 class SessionsController < Devise::SessionsController
   def new
-    logger.info 'HERE'
+    @show_sign_in = true if params[:s] and params[:s].to_s == '1'
     super
-    resource.email = session[:sign_in_up_email] unless resource.blank? or session[:sign_in_up_email].blank?
+  end
+
+  protected
+
+  def build_resource(*args)
+    super
+    @user.email = session[:sign_in_up_email] unless @user.blank? or session[:sign_in_up_email].blank?
   end
 end
