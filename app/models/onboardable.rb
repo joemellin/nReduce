0289adb262e5dcaf60b88cdf3507257fd7937942
Josh Schwartzman <geeosh@gmail.com>
2012-06-30 19:45:37 -0700

    fix onboarding steps

--- app/models/onboardable.rb
+++ app/models/onboardable.rb
@@ -1,10 +1,14 @@
 module Onboardable
   # Add to classes that go through a multi-step onboarding process
-  
+
   def self.included(base)
     # Adding relationships here so it doesn't complain that active_record isn't avail
     base.class_eval do
-      scope :onboarded, lambda {|obj| where(:onboarding_step => obj.num_onboarding_steps) }
+      scope :onboarded, lambda { where(:onboarding_step => self.num_onboarding_steps) }
+    
+      def self.num_onboarding_steps
+        self.new.num_onboarding_steps
+      end
     end
   end
 
