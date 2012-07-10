f866b074f93d95b062a8aa049b5ae4fe0ba41187
Josh Schwartzman <geeosh@gmail.com>
2012-07-10 16:38:48 -0700

    assign week

--- db/migrate/20120710231800_add_week_to_checkin.rb
+++ db/migrate/20120710231800_add_week_to_checkin.rb
@@ -2,7 +2,7 @@ class AddWeekToCheckin < ActiveRecord::Migration
   def change
     add_column :checkins, :week, :integer, :length => 6
 
-    Checkin.all.each |c|
+    Checkin.all.each do |c|
       c.assign_week
       c.save(:validate => false)
     end
