a7a720567c1bbc228405ac3094b41ae8641d47b6
Josh Schwartzman <geeosh@gmail.com>
2012-07-25 00:08:29 -0700

    Adding approve user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,4 +9,9 @@ class Admin::UsersController < ApplicationController
     flash[:notice] = "You are now signed in as #{@user.name}. Remember to sign out when you're done."
     redirect_to @user
   end
+
+  def approve
+    flash[:notice] = "User account has been set up" if @user.setup_complete!
+    redirect_to @user
+  end
 end
