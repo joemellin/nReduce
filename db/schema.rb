6d4727ca875f919c7aa9db3718612a4d7e249b94
Joe Mellin <mellin.joe@gmail.com>
2012-07-25 08:25:05 -0700

    updated nStar page

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20120725071121) do
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
     t.integer  "awesome_count",   :default => 0
+    t.text     "before_comments"
     t.text     "start_comments"
     t.integer  "comment_count",   :default => 0
     t.integer  "week"
@@ -142,13 +143,13 @@ ActiveRecord::Schema.define(:version => 20120725071121) do
 
   create_table "notifications", :force => true do |t|
     t.string   "message"
-    t.string   "action"
     t.integer  "attachable_id"
     t.string   "attachable_type"
     t.integer  "user_id"
     t.boolean  "emailed",         :default => false
     t.datetime "read_at"
     t.datetime "created_at"
+    t.string   "action"
   end
 
   add_index "notifications", ["user_id", "read_at"], :name => "index_notifications_on_user_id_and_read_at"
@@ -186,7 +187,6 @@ ActiveRecord::Schema.define(:version => 20120725071121) do
     t.string   "connected_with_type"
     t.text     "message"
     t.integer  "context"
-    t.string   "reason"
   end
 
   add_index "relationships", ["entity_id", "entity_type", "status"], :name => "relationship_index"
@@ -230,17 +230,6 @@ ActiveRecord::Schema.define(:version => 20120725071121) do
   end
 
   add_index "startups", ["public"], :name => "index_startups_on_public"
-
-  create_table "suggested_startups", :force => true do |t|
-    t.string   "entity_type"
-    t.string   "suggested_entity_type"
-    t.integer  "entity_id"
-    t.integer  "state"
-    t.string   "reason"
-    t.datetime "decided_at"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
-  end
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
@@ -294,7 +283,6 @@ ActiveRecord::Schema.define(:version => 20120725071121) do
     t.string   "location"
     t.float    "lat"
     t.float    "lng"
-    t.boolean  "admin",                  :default => false
     t.boolean  "mailchimped",            :default => false
     t.integer  "startup_id"
     t.datetime "created_at",                                :null => false
@@ -314,11 +302,11 @@ ActiveRecord::Schema.define(:version => 20120725071121) do
     t.string   "intro_video_url"
     t.integer  "roles"
     t.integer  "onboarded"
+    t.boolean  "admin"
     t.integer  "email_on"
     t.integer  "setup"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["roles"], :name => "index_users_on_roles"
   add_index "users", ["startup_id"], :name => "index_users_on_startup_id"
