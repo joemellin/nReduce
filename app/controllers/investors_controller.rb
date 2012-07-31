240ad3b4094c524d46a62ea712d9083d472ab31a
Josh Schwartzman <geeosh@gmail.com>
2012-07-30 21:10:13 -0700

    Investor page - most elements including charts there now

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -9,7 +9,12 @@ class InvestorsController < ApplicationController
   # Show a new startup to an investor
   def show_startup 
     authorize! :investor_connect_with_startups, current_user
-    @startup = Startup.order('RAND()').first
+    @startup = Startup.find 1
+    # search = Startup.search {
+    #   with(:num_checkins).greater_than(1)
+    #   paginate :per_page => 1
+    # }
+    # @startup = search.results.first
     @rating = Rating.new(:startup_id => @startup.id)
     @checkin_history = Checkin.history_for_startup(@startup)
 
