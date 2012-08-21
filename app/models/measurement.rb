fb9809eb94c4ac0aa688be8ecdc9044326d264c2
Josh Schwartzman <geeosh@gmail.com>
2012-08-21 11:32:27 +0800

    Display values on traction chart

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -5,5 +5,7 @@ class Measurement < ActiveRecord::Base
 
   attr_accessible :value, :instrument, :instrument_id, :checkin, :checkin_id
 
+  scope :ordered_asc, order('created_at ASC')
+
   validates_numericality_of :value, :greater_than_or_equal_to => 0
 end
