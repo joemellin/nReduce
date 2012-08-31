8eee0edba66e793c4fceced643181227b0c26e96
Josh Schwartzman <geeosh@gmail.com>
2012-08-31 08:00:51 -0700

    Timeboxing for demo day

--- app/models/demo_day.rb
+++ app/models/demo_day.rb
@@ -7,4 +7,17 @@ class DemoDay < ActiveRecord::Base
     return [] if self.startup_ids.blank?
     Startup.find(self.startup_ids)
   end
+
+  def starts_at
+    Time.parse("#{self.day} 11:00:00 -0700")
+  end
+
+  def ends_at
+    Time.parse("#{self.day} 13:00:00 -0700")
+  end
+
+  # Returns true if it's currently the time window for this demo day
+  def in_time_window?
+    self.starts_at <= Time.now && self.ends_at >= Time.now
+  end
 end
