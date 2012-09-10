d9ee4e613e556f641ae0772e5607dbe97a632ece
Josh Schwartzman <geeosh@gmail.com>
2012-09-10 07:52:05 -0700

    Remove creating weekly classes from migration

--- db/migrate/20120907231314_create_weekly_classes.rb
+++ db/migrate/20120907231314_create_weekly_classes.rb
@@ -11,8 +11,5 @@ class CreateWeeklyClasses < ActiveRecord::Migration
     end
 
     add_index :weekly_classes, :week, :unique => true
-
-    # Create Weekly classes for past users
-    WeeklyClass.populate_for_past_weeks
   end
 end
