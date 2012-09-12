b6960cca19337da06de16a786612df489f7b50b2
Josh Schwartzman <geeosh@gmail.com>
2012-09-12 14:18:43 -0700

    fix startup

--- app/controllers/weekly_classes_controller.rb
+++ app/controllers/weekly_classes_controller.rb
@@ -9,7 +9,11 @@ class WeeklyClassesController < ApplicationController
     @user = current_user
     @in_time_window = @weekly_class.in_join_window?
     if @in_time_window
-      create_startup if @startup.blank?
+      if current_user.startup.blank?
+        create_startup
+      else
+        @startup = current_user.startup
+      end
       # Generates session key for startup and initializes user as moderator if they are a part of the startup
       @nreduce = Startup.find_by_obfuscated_id(Startup.nreduce_id)
       load_data
