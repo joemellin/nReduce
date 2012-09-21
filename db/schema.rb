f141383460699e22ac909bc15c6e427a783c2425
Joe Mellin <mellin.joe@gmail.com>
2012-09-21 16:32:02 -0700

    front end stuff with comments

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
     t.integer  "awesome_count",   :default => 0
+    t.text     "before_comments"
     t.text     "start_comments"
     t.integer  "comment_count",   :default => 0
     t.integer  "week"
@@ -62,11 +63,12 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.text     "content"
     t.integer  "user_id"
     t.integer  "checkin_id"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
     t.integer  "awesome_count", :default => 0
     t.string   "ancestry"
-    t.text     "commenter_ids"
+    t.text     "responder_ids"
+    t.boolean  "deleted",       :default => false
   end
 
   add_index "comments", ["ancestry"], :name => "index_comments_on_ancestry"
@@ -104,11 +106,10 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.integer  "to_id"
     t.integer  "from_id"
     t.integer  "startup_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "name"
     t.datetime "emailed_at"
-    t.integer  "weekly_class_id"
   end
 
   add_index "invites", ["code"], :name => "index_invites_on_code"
@@ -161,13 +162,13 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
 
   create_table "notifications", :force => true do |t|
     t.string   "message"
-    t.string   "action"
     t.integer  "attachable_id"
     t.string   "attachable_type"
     t.integer  "user_id"
     t.boolean  "emailed",         :default => false
     t.datetime "read_at"
     t.datetime "created_at"
+    t.string   "action"
   end
 
   add_index "notifications", ["user_id", "read_at"], :name => "index_notifications_on_user_id_and_read_at"
@@ -179,11 +180,6 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "invite_id"
-  end
-
-  create_table "posts", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "questions", :force => true do |t|
@@ -237,7 +233,6 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.string   "connected_with_type"
     t.text     "message"
     t.integer  "context"
-    t.string   "reason"
     t.datetime "pending_at"
   end
 
@@ -298,29 +293,18 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.string   "pitch_video_url"
     t.integer  "setup"
     t.boolean  "investable",           :default => false
-    t.integer  "week"
-    t.integer  "intro_video_id"
-    t.integer  "pitch_video_id"
     t.text     "business_model"
     t.date     "founding_date"
     t.string   "market_size"
+    t.integer  "week"
+    t.integer  "intro_video_id"
+    t.integer  "pitch_video_id"
     t.string   "tokbox_session_id"
     t.string   "cached_industry_list"
   end
 
   add_index "startups", ["public"], :name => "index_startups_on_public"
   add_index "startups", ["week"], :name => "index_startups_on_week"
-
-  create_table "suggested_startups", :force => true do |t|
-    t.string   "entity_type"
-    t.string   "suggested_entity_type"
-    t.integer  "entity_id"
-    t.integer  "state"
-    t.string   "reason"
-    t.datetime "decided_at"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
-  end
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
@@ -374,7 +358,6 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.string   "location"
     t.float    "lat"
     t.float    "lng"
-    t.boolean  "admin",                  :default => false
     t.boolean  "mailchimped",            :default => false
     t.integer  "startup_id"
     t.datetime "created_at",                                :null => false
@@ -394,6 +377,7 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.string   "intro_video_url"
     t.integer  "roles"
     t.integer  "onboarded"
+    t.boolean  "admin"
     t.integer  "email_on"
     t.integer  "setup"
     t.integer  "intro_video_id"
@@ -404,7 +388,6 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.string   "cached_industry_list"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["roles"], :name => "index_users_on_roles"
   add_index "users", ["startup_id"], :name => "index_users_on_startup_id"
@@ -428,10 +411,10 @@ ActiveRecord::Schema.define(:version => 20120920212732) do
     t.integer  "vimeo_id"
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
+    t.integer  "startup_id"
     t.boolean  "vimeod",          :default => false
     t.string   "type"
     t.string   "title"
-    t.integer  "startup_id"
     t.string   "image"
   end
 
