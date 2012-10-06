7bff2fd429cfeed952e82d8e480efbf62468cae4
Josh Schwartzman <geeosh@gmail.com>
2012-10-06 15:37:45 -0700

    Commented on turns green when commented on, as well as share button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@ class PostsController < ApplicationController
     connection_ids << current_user.startup_id
     # Load all comments from this person's 1st and 2nd degree connections, as well as all original posts if these are reposts
     # Groups by original id so we don't get dupes from shares
-    @comments = Comment.posts.where(:startup_id => connection_ids).includes(:original).group('comments.original_id').limit(30).order('created_at DESC').all
-    @hottest_post = Comment.last #Comment.hottest_post_for_time(Time.now)
+    @comments = Comment.posts.where(:startup_id => connection_ids).includes(:original).group('comments.original_id').limit(30).order('updated_at DESC').all
+    @hottest_post = Comment.hottest_post_for_time(Time.now)
   end
 
   def show
@@ -20,12 +20,9 @@ class PostsController < ApplicationController
   def repost
     original = Comment.find(params[:id])
     repost = original.repost_by(current_user)
-    if repost.errors.blank?
-      flash[:notice] = "You've reposted this post"
-      redirect_to post_path(repost)
-    else
-      flash[:alert] = "Could not repost: #{repost.errors.full_messages}"
-      redirect_to post_path(original)
+    @success = true if repost.errors.blank?
+    respond_to do |format|
+      format.js
     end
   end
 end
