1c1ce9125836a54f520935daa27c63ad7ceea2a3
Josh Schwartzman <geeosh@gmail.com>
2012-10-08 12:13:50 -0700

    Change video quality to medium

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_filter :login_required
   load_and_authorize_resource :comment, :parent => false
-  before_filter :load_requested_or_users_startup
 
   def index
     connection_ids = current_user.startup.second_degree_connection_ids
@@ -10,11 +9,13 @@ class PostsController < ApplicationController
     # Groups by original id so we don't get dupes from shares
     @comments = Comment.posts.where(:startup_id => connection_ids).includes(:original).group('comments.original_id').limit(30).order('updated_at DESC').all
     @hottest_post = Comment.hottest_post_for_time(Time.now)
+    @startup = current_user.startup
   end
 
   def show
     authorize! :read_post, Comment
     @comments = @comment.descendants.includes(:user).arrange(:order => 'created_at DESC') # arrange in nested order
+    @startup = current_user.startup
   end
 
   def repost
