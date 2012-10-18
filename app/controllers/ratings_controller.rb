c415b23ff2e94866fcb7956624e18d2841c3314a
Josh Schwartzman <geeosh@gmail.com>
2012-10-18 17:10:04 +0100

    fix loading of startups for mentor

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -20,7 +20,7 @@ class RatingsController < ApplicationController
       render :action => :entrepreneur
     else
       authorize! :see_ratings_page, current_user
-      startups = Hash.by_key(current_user.connected_to, :id)
+      startups = current_user.connected_to
       #startups = Startup.limit(10).all + [Startup.find(319)]
       @checkins_by_week = Checkin.for_startups_by_week(startups, 20)
       @startups_by_id =  Hash.by_key(startups, :id)
