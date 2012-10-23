859e5fe2efbac85813dde255028c743046417a17
Josh Schwartzman <geeosh@gmail.com>
2012-10-23 16:12:09 +0100

    Reduce timeframe to 2 months

--- app/controllers/admin/metrics_controller.rb
+++ app/controllers/admin/metrics_controller.rb
@@ -5,7 +5,7 @@ class Admin::MetricsController < ApplicationController
   def index
     @checkin_data = Stats.checkins_per_week_for_chart(2.months)
     @comment_data = Stats.comments_per_week_for_chart(2.months)
-    @comments_per_checkin_data = Stats.comments_per_checkin_for_chart(1.year)
+    @comments_per_checkin_data = Stats.comments_per_checkin_for_chart(2.months)
     @active_teams_data = Stats.connections_per_startup_for_chart(2.months)
     @activation_data = Stats.startups_activated_per_week_for_chart(2.months)
     @retention_data = Stats.weekly_retention_for_chart(1.year) # do this one for all time
