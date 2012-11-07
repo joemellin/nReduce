7076f8951acb3ef7d300b5026a7d20d809d554c0
Josh Schwartzman <geeosh@gmail.com>
2012-11-07 18:42:49 +0000

    show startup

--- app/controllers/demo_day_controller.rb
+++ app/controllers/demo_day_controller.rb
@@ -30,7 +30,7 @@ class DemoDayController < ApplicationController
     @after = true
     if @demo_day.includes_startup?(@startup)
       # Load all checkins made before demo day
-      @checkins = @startup.checkins.where(['created_at < ?', "#{@demo_day.day} 00:00:00"]).ordered.includes(:before_video, :after_video)
+      #@checkins = @startup.checkins.where(['created_at < ?', "#{@demo_day.day} 00:00:00"]).ordered.includes(:before_video, :after_video)
     else
       redirect_to :action => :index
       return
