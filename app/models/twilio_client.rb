efceaa99259f0fde799a10f838b507620b872f94
Josh Schwartzman <geeosh@gmail.com>
2012-11-11 13:44:55 +0000

    call

--- app/models/twilio_client.rb
+++ app/models/twilio_client.rb
@@ -3,7 +3,7 @@ class TwilioClient
 
   def self.account
     return @@twilio_account unless @@twilio_account.blank?
-    @client = Twilio::REST::Client.new(Settings.twilio.sid, Settings.twilio.token)
+    @client = Twilio::REST::Client.new(Settings.apis.twilio.sid, Settings.apis.twilio.token)
     @@twilio_account = @client.account
   end
 end
