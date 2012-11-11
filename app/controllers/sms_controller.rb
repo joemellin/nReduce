5d658123ce2638cd24e6950f5c34bcd9da00407f
Josh Schwartzman <geeosh@gmail.com>
2012-11-11 14:17:47 +0000

    Update call

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,8 +1,11 @@
 class SmsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  
   def receive
     message_body = params["Body"]
     from_number = params["From"]
 
     Call.respond_to_message(from_number, message_body)
+    render :nothing => true
   end
 end
