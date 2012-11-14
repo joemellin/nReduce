79ab9ccbb4aa11a726f0e475e20886bc0da8ca9c
Josh Schwartzman <geeosh@gmail.com>
2012-11-14 15:03:19 +0000

    Clean old session ids in memory

--- app/models/ab_test.rb
+++ app/models/ab_test.rb
@@ -5,7 +5,7 @@ class AbTest < ActiveRecord::Base
   # Returns :a or :b for this session id
 
   def self.version_for_session_id(ab_test_id, session_id)
-    key = "ab_test_#{ab_test_id}"
+    key = AbTest.key_for_id(ab_test_id)
 
     # Check if user has already seen this test
     index = Cache.set_index_of(key, session_id)
@@ -19,5 +19,18 @@ class AbTest < ActiveRecord::Base
 
     # And now return a or b depending on number of items
     return (index % 2 == 0) ? :a : :b
+  end
+
+  # Will go out and clear all but the last 100 session ids
+  def self.clean_old_session_ids(ab_test_id = 1)
+    key = AbTest.key_for_id(ab_test_id)
+    count = Cache.set_count(key)
+    return "Not cleaning: only #{count} session ids are stored." if count <= 100
+    Cache.set_delete_by_index(AbTest.key_for_id(ab_test_id), 0, count - 100)
+    return "Cleaned #{count - 100} session ids."
+  end
+
+  def self.key_for_id(ab_test_id)
+    "ab_test_#{ab_test_id}"
   end
 end
