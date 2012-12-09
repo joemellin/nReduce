505b13bcc5987b017d196b4d173d59776ae1494b
Josh Schwartzman <geeosh@gmail.com>
2012-12-09 11:42:15 +0100

    Tests for request completed

--- spec/factories/slide_decks.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :slide_deck do
-    startup_id 1
-    slides "MyText"
-    title "MyString"
-  end
-end
