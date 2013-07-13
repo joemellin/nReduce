f4045d0e7020d9a80d7e80b081fa7b04f854c9e3
Jacques Crocker <railsjedi@gmail.com>
2013-07-13 14:56:33 -0700

    new redis thing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+uri = URI.parse(ENV["REDISTOGO_URL"] || 'redis://localhost:6379')
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
